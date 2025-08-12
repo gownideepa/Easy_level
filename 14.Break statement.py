7ddbf0b0cefe04997f3fa8cf85536e802c2b1170
Gowni Deepa <gownideepagaaru@gmail.com>
2025-08-12 23:41:19 +0530

    Update 14.Break statement.py

--- 14.Break statement.py
+++ 14.Break statement.py
@@ -1,4 +1,4 @@
-n=int(input("enter number:"))#take input from user
+n=int(input("Enter number:"))#take input from user
 for  i in range(1,n):
 #if i value is equals to 10 then it will come out of the loop
     if i==10:
