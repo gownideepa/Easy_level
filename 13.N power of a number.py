29465f973d4fdd93dcf832dcc2ef0ff4f1079b7b
Gowni Deepa <gownideepagaaru@gmail.com>
2025-09-23 20:57:18 +0530

    Update 13.N power of a number.py

--- 13.N power of a number.py
+++ 13.N power of a number.py
@@ -2,5 +2,10 @@ base=int(input("Base:"))
 exponent=int(input("Exponent:"))
 import math
 print(pow(base,exponent))#implies base to the power exponent
+
+
+
+
+Method--2
 #without math library
 print("Power: ",base**exponent)
